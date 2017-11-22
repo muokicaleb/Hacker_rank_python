945540b5b78defae55b9b461b780e04d632e7f2a
muokicaleb <muokicaleb@gmail.com>
2017-11-22 20:52:55 +0300

    sets

---
+++ sets/check_subset.py
@@ -0,0 +1,46 @@
+"""
+You are given two sets, A and B. 
+Your job is to find whether set A is a subset of set B.
+
+If set A is subset of set B, print True.
+If set A is not a subset of set B, print False.
+
+Input Format
+
+The first line will contain the number of test cases, T. 
+The first line of each test case contains the number of elements in set A.
+The second line of each test case contains the space separated elements of set A.
+The third line of each test case contains the number of elements in set B.
+The fourth line of each test case contains the space separated elements of set B.
+
+ 
+
+Output Format
+
+Output True or False for each test case on separate lines.
+
+Sample Input
+
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
+Sample Output
+
+True 
+False
+False
+"""
+for i in range(int(input())): #More than 4 lines will result in 0 score. Blank lines won't be counted. 
+    a = int(input()); A = set(input().split()) 
+    b = int(input()); B = set(input().split())
+    print(not bool(A.difference(B)))
